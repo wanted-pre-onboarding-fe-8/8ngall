193abafd41cb419d816f6bdcc472793f837cf801
ohtaekwon <ohtk1119@gmail.com>
2022-07-28 15:58:59 +0900

    chore: WEEKDAYS상수 파일 생성

---
+++ src/utils/constants/days.ts
@@ -0,0 +1,9 @@
+export const WEEKDAYS = {
+  MONDAY: 'monday',
+  TUESDAY: 'tuesday',
+  WEDNESDAY: 'wednesday',
+  THURSDAY: 'thursday',
+  FRIDAY: 'friday',
+  SATURDAY: 'saturday',
+  SUNDAY: 'sunday',
+} as const;
