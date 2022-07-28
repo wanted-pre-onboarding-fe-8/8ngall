88f08af18631e77aef5570f26ec89663b0802e85
corgi-world <developer.msw@gmail.com>
2022-07-28 23:08:54 +0900

    feat: 상수 및 time format 변경 함수 구현

---
+++ src/utils/dateTimeHelper.ts
@@ -0,0 +1,71 @@
+import { format, addMinutes } from 'date-fns';
+
+export const HOURS = ['12', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11'];
+export const MINUTES = ['00', '05', '10', '15', '20', '25', '30', '35', '40', '45', '50', '55'];
+export const AM = 'AM';
+export const PM = 'PM';
+export type typeTimeType = typeof AM | typeof PM;
+
+export function calcEndTime(string12: string) {
+  const now = string12ToObject(string12);
+  const end = addMinutes(now, 40);
+  return objectToString12(end);
+}
+
+export function string12ToObject(string12: string) {
+  const [hour, minute, timeType] = splitString12(string12);
+
+  const now = new Date();
+  const offset = timeType === PM ? (+hour !== 12 ? 12 : 0) : +hour === 12 ? -12 : 0;
+  now.setHours(+hour + offset);
+  now.setMinutes(+minute);
+  now.setSeconds(0);
+  return now;
+}
+export function objectToString12(date: Date) {
+  const hour = date.getHours();
+  const timeType = hour < 12 ? AM : PM;
+  return `${format(date, 'hh:mm')} ${timeType}`;
+}
+
+export function string24ToObject(string24: string) {
+  const [hour, minute] = splitString24(string24);
+
+  const now = new Date();
+  now.setHours(+hour);
+  now.setMinutes(+minute);
+  now.setSeconds(0);
+  return now;
+}
+export function objectToString24(date: Date) {
+  return `${format(date, 'HH:mm')}`;
+}
+
+export function string12ToString24(string12: string) {
+  const date = string12ToObject(string12);
+  return objectToString24(date);
+}
+export function string24ToString12(string24: string) {
+  const date = string24ToObject(string24);
+  return objectToString12(date);
+}
+
+export function splitString12(string12: string) {
+  const time = string12.split(' ')[0];
+  const timeType = string12.split(' ')[1];
+  const hour = time.split(':')[0];
+  const minute = time.split(':')[1];
+  return [hour, minute, timeType];
+}
+export function combineString12(timeType: typeTimeType, hour: string, minute: string) {
+  return `${hour}:${minute} ${timeType}`;
+}
+
+export function splitString24(string24: string) {
+  const hour = string24.split(':')[0];
+  const minute = string24.split(':')[1];
+  return [hour, minute];
+}
+export function combineString24(hour: string, minute: string) {
+  return `${hour}:${minute}`;
+}
