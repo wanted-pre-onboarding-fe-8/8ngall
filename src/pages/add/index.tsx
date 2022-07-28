48feef2cbcadc98109c3adcedbd31dedff3b6ee8
bebusl <bebus1998@naver.com>
2022-07-28 19:23:41 +0900

    style: 체크박스 스타일링

--- src/pages/add/index.tsx
+++ src/pages/add/index.tsx
@@ -58,8 +58,8 @@ export default function Add() {
           <Div>
             {WEEKS.map((week) => (
               <span key={week}>
-                <input type='checkbox' id={week} value={week} onChange={handleClick} disabled />
-                <label htmlFor={week}>{week}</label>
+                <Checkbox id={week} value={week} onChange={handleClick} disabled />
+                <CheckboxLabel htmlFor={week}>{week}</CheckboxLabel>
               </span>
             ))}
           </Div>
@@ -116,4 +116,29 @@ const Container = styled.div`
 const Title = styled.div`
   font-size: 20px;
   font-weight: 700;
+`;
+
+const Checkbox = styled.input.attrs({ type: 'checkbox' })<{ disabled: boolean }>`
+  display: none;
+  &:checked + label {
+    background-color: #b1b1b1;
+    color: white;
+  }
+  &:disabled + label {
+    cursor: default;
+    color: #b1b1b1;
+  }
+`;
+
+const CheckboxLabel = styled.label`
+  display: inline-block;
+  width: 130px;
+  margin-right: 5px;
+  height: 30px;
+  outline: 0;
+  text-align: center;
+  background-color: #fff;
+  border: 0.5px solid #b1b1b1;
+  padding: 5px;
+  cursor: pointer;
 `;
