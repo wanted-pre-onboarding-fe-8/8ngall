5547a5b3bb0cf68a4853cd74f2bb761809ab88f7
ChuYeonBin <cndusqls98@gmail.com>
2022-07-29 21:50:57 +0900

    fix: 추가 or 삭제 요청 후 업데이트 된 데이터 가져오기

--- src/queries/schedule.ts
+++ src/queries/schedule.ts
@@ -1,18 +1,34 @@
 import httpClient from '../http';
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { ISchedule, ServerSideScheduleWrapper } from '../types';
 
 export function useGetSchedules() {
   const query = '';
-  return useQuery(['schedules'], () =>
-    httpClient.get<ServerSideScheduleWrapper>(`schedules${query}`),
+  return useQuery(
+    ['schedules'],
+    () => httpClient.get<ServerSideScheduleWrapper>(`schedules${query}`),
+    {
+      staleTime: 1000 * 60,
+    },
   );
 }
 
 export function useAddSchedule() {
-  return useMutation((data: ISchedule) => httpClient.post('schedules', data));
+  const queryClient = useQueryClient();
+
+  return useMutation((data: ISchedule) => httpClient.post('schedules', data), {
+    onSuccess: () => {
+      queryClient.invalidateQueries(['schedules']);
+    },
+  });
 }
 
 export function useDeleteScheduleById() {
-  return useMutation((id: number) => httpClient.delete(`schedules/${id}`));
+  const queryClient = useQueryClient();
+
+  return useMutation((id: number) => httpClient.delete(`schedules/${id}`), {
+    onSuccess: () => {
+      queryClient.invalidateQueries(['schedules']);
+    },
+  });
 }
